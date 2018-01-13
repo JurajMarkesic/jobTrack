dc8754e7f46085653e9a0fa340f4a3b6b74b6bd7
Juraj <jmarkesic@tvz.hr>
2018-01-13 13:41:24 +0100

    Edited config/database.php with heroku db details.

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'jooble'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_9253a3b4a12d535'),
+            'username' => env('DB_USERNAME', 'b70d50e9cbb6d6'),
+            'password' => env('DB_PASSWORD', '9f79061c'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
